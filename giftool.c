c4cb9231990ac7f1616971f27875fbad92b4392a
karl-police <karl-police2001@bluewin.ch>
2021-03-11 00:10:39 +0100

    Update giftool.c

--- giftool.c
+++ giftool.c
@@ -8,7 +8,6 @@ SPDX-License-Identifier: MIT
 
 #include <stdio.h>
 #include <stdlib.h>
-#define _DEFAULT_SOURCE 1
 #include <string.h>
 #include <fcntl.h>
 #include <stdbool.h>
@@ -67,6 +66,32 @@ bool getbool(char *from)
 		  "giftool: %s is not a valid boolean argument.\n",
 		  sp->name);
     exit(EXIT_FAILURE);
+}
+
+char *strsep(char **stringp, const char *delim)
+{
+	char *s;
+	const char *spanp;
+	int c, sc;
+	char *tok;
+
+	if ((s = *stringp) == NULL)
+		return (NULL);
+	for (tok = s;;) {
+		c = *s++;
+		spanp = delim;
+		do {
+			if ((sc = *spanp++) == c) {
+				if (c == 0)
+					s = NULL;
+				else
+					s[-1] = 0;
+				*stringp = s;
+				return (tok);
+			}
+		} while (sc != 0);
+	}
+	/* NOTREACHED */
 }
 
 struct operation {
